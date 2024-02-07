d832cec46c17d05d481a38c75bf4665a81689c1c
simran <bhattsimran939@gmail.com>
2024-02-07 15:38:04 +0530

    xyz

--- basic/xyz.py
+++ basic/xyz.py
@@ -1,6 +1,7 @@
 import mysql.connector\
-mydb=mysql.connector.connect(host="localhost",
+
+mydb1=mysql.connector.connect(host="localhost",
                              user="root",
                              password="root",
                              database="tender_management_system")
-print(mydb)
+print(mydb1)
